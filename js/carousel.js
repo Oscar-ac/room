2e05d0a911c127c59a0c52e9c70bc8cb1d7ff00f
Oscar A.C <oangelkovcummings@gmail.com>
2022-12-07 13:40:02 +0900

    big update and progress

---
+++ js/carousel.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  console.log("hello world");
+
+  var index = 0;
+
+  const imageContainer = document.querySelector(".slide-image");
+  const textContainer = document.querySelector(".slide-text");
+  const titleConatiner = document.querySelector(".slide-title");
+
+  const content = [{ 
+      title: "Discover innovative ways to decorate",
+      text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+      img: "./images/desktop-image-hero-1.jpg"
+    },
+    { 
+      title: "We are available all across the globe",
+      text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+      img: "./images/desktop-image-hero-2.jpg"
+    },
+    { 
+      title: "Manufactured with the best materials",
+      text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+      img: "./images/desktop-image-hero-3.jpg"
+    }];
+  console.log(content);
+
+  const right = document.querySelector("#button-right");
+  const left = document.querySelector("#button-left");
+
+  left.addEventListener("click", function(event){
+    console.log(event.target);
+    index--;
+
+    if(index < 0){
+      index = content.length - 1;
+    }
+
+    addContent(index);
+    console.log(index);
+
+
+  });
+  right.addEventListener("click", function(event){
+    console.log(event.target);
+    index++;
+
+
+    if(index > content.length - 1){
+      index = 0;
+    }
+
+    addContent(index);
+    console.log(index);
+  });
+
+
+  const addContent = (i) => {
+    console.log(imageContainer);
+
+    imageContainer.src = content[i].img;
+    textContainer.innerHTML = content[i].text;
+    titleConatiner.innerHTML = content[i].title;
+
+  }
+
+  // addContent(0);
+});
+
