669f63b7144388a0fc0e105afcd63c995fa7b419
Oscar A.C <oangelkovcummings@gmail.com>
2023-02-05 21:08:43 +0900

    updated to include alt text and change buttons to divs

--- js/carousel.js
+++ js/carousel.js
@@ -15,18 +15,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
       text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
       img: "./images/desktop-image-hero-1.jpg",
       imgMobile: "./images/mobile-image-hero-1.jpg",
+      altText: "Stylish white chairs and table with a bonsai tree on top."
     },
     { 
       title: "We are available all across the globe",
       text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
       img: "./images/desktop-image-hero-2.jpg",
       imgMobile: "./images/mobile-image-hero-2.jpg",
+      altText: "3 modern looking dining chairs with varying colours"
     },
     { 
       title: "Manufactured with the best materials",
       text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
       img: "./images/desktop-image-hero-3.jpg",
       imgMobile: "./images/mobile-image-hero-3.jpg",
+      altText: "black folding chair on a grey background"
     }];
   console.log(content);
 
@@ -76,6 +79,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     setTimeout(() => {
       imageContainer.src = content[i].img;
       imageMobileContainer.src = content[i].imgMobile;
+      imageContainer.alt = content[i].altText;
+      imageMobileContainer.alt = content[i].altText;
       textContainer.innerHTML = content[i].text;
       titleContainer.innerHTML = content[i].title;
       imageContainer.classList.remove("opacity-0");
